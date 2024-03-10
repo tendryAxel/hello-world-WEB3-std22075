60a284eaaf525b940675575e295bbb74f6f1e77b
tendryAxel <hei.tendry@gmail.com>
2024-03-10 19:33:31 +0300

    feat: footer

--- app/test/layout.tsx
+++ app/test/layout.tsx
@@ -13,11 +13,15 @@ export default ({
             {children}
             <footer>
                 <div>
-                    <h1></h1>
-                    <p></p>
+                    <h3>App</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nostrum, itaque quae tenetur quia amet sed quibusdam necessitatibus voluptate ullam repellat architecto laborum tempora possimus ea illum sequi non nobis.</p>
                 </div>
                 <div>
-                    <input type="text" placeholder="Email" />
+                    <h3>Comment</h3>
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Email" />
                 </div>
             </footer>
         </div>
